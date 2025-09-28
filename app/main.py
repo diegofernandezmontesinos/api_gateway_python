9b0b30bf87683db02b24b62137f1f0ca406e878f
Diego <dfmontesinos@gmail.com>
2025-09-28 21:55:51 +0200

    Created basic htmland routes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
 
 app = FastAPI()
 app.include_router(router)
+
+# 🚀 Middleware de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ⚠️ acepta cualquier origen en desarrollo
+    allow_credentials=True,
+    allow_methods=["*"],  # permite GET, POST, PUT, DELETE, OPTIONS
+    allow_headers=["*"],  # permite todos los headers, incluido X-API-KEY
+)
